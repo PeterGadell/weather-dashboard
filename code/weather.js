1126829b89c4a507ac2e5a13547f49dfe617d22a
Emelie Sidesiö <esidesio@gmail.com>
2017-11-23 13:48:50 +0100

    background change - stretch 1

--- code/weather.js
+++ code/weather.js
@@ -4,4 +4,9 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=m
   document.getElementById("city").innerHTML = (json.name)
   document.getElementById("temperature").innerHTML = (json.main.temp) + "°C"
   document.getElementById("type").innerHTML = (json.weather[0].description)
+  if (json.main.temp < 10) {
+    document.getElementById("body").classList.toggle("cold")
+} if (json.main.temp > 10) {
+    document.getElementById("body").classList.toggle("warm")
+}
 })
